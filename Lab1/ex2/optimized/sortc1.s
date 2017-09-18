a5226cf7f8cf5687e6b6fc445af578be28374134
Day <dayannefernandesc@gmail.com>
2017-09-18 20:07:42 -0300

    fix sintax error sortc with directive -O1

--- Lab1/ex2/optimized/sortc1.s
+++ Lab1/ex2/optimized/sortc1.s
@@ -1,25 +1,25 @@
-	.file	1 "sortc.c"
-	.section .mdebug.abi32
-	.previous
-	.nan	legacy
-	.gnu_attribute 4, 1
-	.section	.rodata.str1.4,"aMS",@progbits,1
-	.align	2
-$LC0:
-	.ascii	"%d\011\000"
-	.text
-	.align	2
-	.globl	show
-	.set	nomips16
-	.set	nomicromips
-	.ent	show
-	.type	show, @function
+.text
+main:
+	# TODO fix flow logic
+	addiu	$sp,$sp,-24
+	sw	$31,20($sp)
+	sw	$16,16($sp)
+	la	$4,v
+	jal	show
+	li	$5,10			# 0xa
+
+	la	$4,v
+	jal	sort
+	li	$5,10			# 0xa
+
+	la	$4,v
+	jal	show
+	li	$5,10			# 0xa
+
+	li	$2, 10
+	syscall
+	
 show:
-	.frame	$sp,40,$31		# vars= 0, regs= 5/0, args= 16, gp= 0
-	.mask	0x800f0000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	addiu	$sp,$sp,-40
 	sw	$31,36($sp)
 	sw	$19,32($sp)
@@ -31,11 +31,10 @@ show:
 
 	move	$17,$4
 	move	$16,$0
-	lui	$19,%hi($LC0)
-	addiu	$19,$19,%lo($LC0)
+	la	$19,$LC0
 $L3:
 	move	$4,$19
-	jal	printf
+	# jal	printf
 	lw	$5,0($17)
 
 	addiu	$16,$16,1
@@ -43,7 +42,7 @@ $L3:
 	addiu	$17,$17,4
 
 $L2:
-	jal	putchar
+	#jal	putchar
 	li	$4,10			# 0xa
 
 	lw	$31,36($sp)
@@ -51,25 +50,10 @@ $L2:
 	lw	$18,28($sp)
 	lw	$17,24($sp)
 	lw	$16,20($sp)
-	j	$31
+	jr	$31
 	addiu	$sp,$sp,40
 
-	.set	macro
-	.set	reorder
-	.end	show
-	.size	show, .-show
-	.align	2
-	.globl	swap
-	.set	nomips16
-	.set	nomicromips
-	.ent	swap
-	.type	swap, @function
 swap:
-	.frame	$sp,0,$31		# vars= 0, regs= 0/0, args= 0, gp= 0
-	.mask	0x00000000,0
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	sll	$5,$5,2
 	addu	$2,$4,$5
 	lw	$3,0($2)
@@ -77,25 +61,10 @@ swap:
 	addu	$4,$4,$5
 	lw	$5,0($4)
 	sw	$5,0($2)
-	j	$31
+	jr	$31
 	sw	$3,0($4)
 
-	.set	macro
-	.set	reorder
-	.end	swap
-	.size	swap, .-swap
-	.align	2
-	.globl	sort
-	.set	nomips16
-	.set	nomicromips
-	.ent	sort
-	.type	sort, @function
 sort:
-	.frame	$sp,48,$31		# vars= 0, regs= 8/0, args= 16, gp= 0
-	.mask	0x807f0000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
 	blez	$5,$L14
 	nop
 
@@ -158,55 +127,12 @@ $L8:
 	lw	$16,16($sp)
 	addiu	$sp,$sp,48
 $L14:
-	j	$31
+	jr	$31
 	nop
 
-	.set	macro
-	.set	reorder
-	.end	sort
-	.size	sort, .-sort
-	.align	2
-	.globl	main
-	.set	nomips16
-	.set	nomicromips
-	.ent	main
-	.type	main, @function
-main:
-	.frame	$sp,24,$31		# vars= 0, regs= 2/0, args= 16, gp= 0
-	.mask	0x80010000,-4
-	.fmask	0x00000000,0
-	.set	noreorder
-	.set	nomacro
-	addiu	$sp,$sp,-24
-	sw	$31,20($sp)
-	sw	$16,16($sp)
-	lui	$16,%hi(v)
-	addiu	$4,$16,%lo(v)
-	jal	show
-	li	$5,10			# 0xa
-
-	addiu	$4,$16,%lo(v)
-	jal	sort
-	li	$5,10			# 0xa
-
-	addiu	$4,$16,%lo(v)
-	jal	show
-	li	$5,10			# 0xa
-
-	lw	$31,20($sp)
-	lw	$16,16($sp)
-	j	$31
-	addiu	$sp,$sp,24
-
-	.set	macro
-	.set	reorder
-	.end	main
-	.size	main, .-main
-	.globl	v
-	.data
-	.align	2
-	.type	v, @object
-	.size	v, 40
+.data
+$LC0:
+	.ascii	"\n"
 v:
 	.word	5
 	.word	8
@@ -218,4 +144,3 @@ v:
 	.word	0
 	.word	1
 	.word	9
-	.ident	"GCC: (GNU) 4.8.1"
