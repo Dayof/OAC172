d1863e154c035cf7d050d2183d44ced379c35c3f
Day <dayannefernandesc@gmail.com>
2017-09-18 19:41:36 -0300

    restart opt files

--- Lab1/ex2/optimized/sortc0.s
+++ Lab1/ex2/optimized/sortc0.s
@@ -1,6 +1,13 @@
+	.file	1 "sortc.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.gnu_attribute 4, 1
 	.globl	v
 	.data
 	.align	2
+	.type	v, @object
+	.size	v, 40
 v:
 	.word	5
 	.word	8
@@ -12,13 +19,23 @@ v:
 	.word	0
 	.word	1
 	.word	9
+	.rdata
 	.align	2
 $LC0:
 	.ascii	"%d\011\000"
+	.text
 	.align	2
 	.globl	show
-.text
+	.set	nomips16
+	.set	nomicromips
+	.ent	show
+	.type	show, @function
 show:
+	.frame	$fp,32,$31		# vars= 8, regs= 2/0, args= 16, gp= 0
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
 	addiu	$sp,$sp,-32
 	sw	$31,28($sp)
 	sw	$fp,24($sp)
@@ -26,7 +43,7 @@ show:
 	sw	$4,32($fp)
 	sw	$5,36($fp)
 	sw	$0,16($fp)
-	jal	$L2
+	j	$L2
 	nop
 
 $L3:
@@ -35,11 +52,10 @@ $L3:
 	lw	$3,32($fp)
 	addu	$2,$3,$2
 	lw	$2,0($2)
-	la	$4, $LC0
-	#lui	$3,%hi($LC0)
-	#addiu	$4,$3,%lo($LC0)
+	lui	$3,%hi($LC0)
+	addiu	$4,$3,%lo($LC0)
 	move	$5,$2
-	#jal	printf
+	jal	printf
 	nop
 
 	lw	$2,16($fp)
@@ -53,20 +69,32 @@ $L2:
 	nop
 
 	li	$4,10			# 0xa
-	#syscall
-	#jal	putchar
+	jal	putchar
 	nop
 
 	move	$sp,$fp
-	#lw	$31,28($sp)
+	lw	$31,28($sp)
 	lw	$fp,24($sp)
 	addiu	$sp,$sp,32
-	jr	$31
+	j	$31
 	nop
 
-	#.align	2
+	.set	macro
+	.set	reorder
+	.end	show
+	.size	show, .-show
+	.align	2
 	.globl	swap
+	.set	nomips16
+	.set	nomicromips
+	.ent	swap
+	.type	swap, @function
 swap:
+	.frame	$fp,16,$31		# vars= 8, regs= 1/0, args= 0, gp= 0
+	.mask	0x40000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
 	addiu	$sp,$sp,-16
 	sw	$fp,12($sp)
 	move	$fp,$sp
@@ -99,14 +127,27 @@ swap:
 	move	$sp,$fp
 	lw	$fp,12($sp)
 	addiu	$sp,$sp,16
-	jr	$31
+	j	$31
 	nop
 
-	#.align	2
+	.set	macro
+	.set	reorder
+	.end	swap
+	.size	swap, .-swap
+	.align	2
 	.globl	sort
+	.set	nomips16
+	.set	nomicromips
+	.ent	sort
+	.type	sort, @function
 sort:
+	.frame	$fp,32,$31		# vars= 8, regs= 2/0, args= 16, gp= 0
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
 	addiu	$sp,$sp,-32
-	#sw	$31,28($sp)
+	sw	$31,28($sp)
 	sw	$fp,24($sp)
 	move	$fp,$sp
 	sw	$4,32($fp)
@@ -163,43 +204,59 @@ $L6:
 	nop
 
 	move	$sp,$fp
-	#lw	$31,28($sp)
+	lw	$31,28($sp)
 	lw	$fp,24($sp)
 	addiu	$sp,$sp,32
-	jr	$31
+	j	$31
 	nop
 
-	#.align	2
+	.set	macro
+	.set	reorder
+	.end	sort
+	.size	sort, .-sort
+	.align	2
 	.globl	main
+	.set	nomips16
+	.set	nomicromips
+	.ent	main
+	.type	main, @function
 main:
+	.frame	$fp,24,$31		# vars= 0, regs= 2/0, args= 16, gp= 0
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
 	addiu	$sp,$sp,-24
-	#sw	$31,20($sp)
+	sw	$31,20($sp)
 	sw	$fp,16($sp)
 	move	$fp,$sp
-	la	$4, v
-	#lui	$2,%hi(v)
-	#addiu	$4,$2,%lo(v)
+	lui	$2,%hi(v)
+	addiu	$4,$2,%lo(v)
 	li	$5,10			# 0xa
 	jal	show
 	nop
 
-	la	$4, v
-	#lui	$2,%hi(v)
-	#addiu	$4,$2,%lo(v)
+	lui	$2,%hi(v)
+	addiu	$4,$2,%lo(v)
 	li	$5,10			# 0xa
 	jal	sort
 	nop
 
-	la	$4, v
-	#lui	$2,%hi(v)
-	#addiu	$4,$2,%lo(v)
+	lui	$2,%hi(v)
+	addiu	$4,$2,%lo(v)
 	li	$5,10			# 0xa
 	jal	show
 	nop
 
 	move	$sp,$fp
-	#lw	$31,20($sp)
+	lw	$31,20($sp)
 	lw	$fp,16($sp)
 	addiu	$sp,$sp,24
-	jr	$31
+	j	$31
 	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.1"
